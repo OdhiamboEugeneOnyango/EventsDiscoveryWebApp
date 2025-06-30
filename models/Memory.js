8b6fe47e74113f4973035a7f06d2a8824c4f2328
Mornylyne <mornylyne.wafula@strathmore.edu>
2025-06-30 15:08:02 +0300

    my change

--- models/Memory.js
+++ models/Memory.js
@@ -1,18 +1,19 @@
 // models/Memory.js
 const mongoose = require('mongoose');
 
+// models/Memory.js
 const memorySchema = new mongoose.Schema({
     eventId: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true },
     eventName: { type: String, required: true },
     eventDate: { type: String, required: true },
     location: { type: String, required: true },
-    caption: { type: String },
+    caption: { type: String, default: '' }, // Added default
     mediaUrl: { type: String, required: true },
     type: { type: String, enum: ['image', 'video'], required: true },
     likes: { type: Number, default: 0 },
     likedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-    
-    comments: { type: Number, default: 0 }
+    comments: { type: Number, default: 0 },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' } 
 }, {
     timestamps: true
 });
